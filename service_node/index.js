c463bfe56394f2089012a946eb6abced4a90536e
aniketpuro <aniketpurohit851@gmail.com>
2025-07-06 22:49:19 +0530

    Removed node_modules from repo and added .gitignore

--- service_node/index.js
+++ service_node/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
+const port = 5001;
 
-app.get('/node', (req, res) => {
-  res.json({ message: 'Hello from Node.js!' });
+// Create a custom metric
+const responseTime = new client.Histogram({
+  name: 'node_response_time_seconds',
+  help: 'Response time in seconds',
+  labelNames: ['method', 'route', 'status_code']
 });
 
-app.get('/metrics', (req, res) => {
-  res.send(`node_response_time_seconds ${Math.random() * 2}`);
+// Middleware to record response time
+app.use((req, res, next) => {
+  const end = responseTime.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, status_code: res.statusCode });
+  });
+  next();
 });
 
-app.listen(5001, () => {
-  console.log('Node service running on port 5001');
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+
+// Expose metrics
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`Node service listening at http://localhost:${port}`);
 });
